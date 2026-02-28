9681568374a0b422cff40e21586afdb5a23e0e28
ShuaiLYU <shuai.lyu@foxmail.com>
2026-02-28 22:54:25 +0800

    Update predict_yoloe_tp and predict_yoloe_vp

--- ultra_ext/yoloe.py
+++ ultra_ext/yoloe.py
@@ -85,42 +85,55 @@ class TestSample:
 		return cls.text_prompts[index]
 
 
+from ultralytics import YOLO,YOLOE
+
 def predict_yoloe_tp(model_weight="yoloe-26l-seg.pt", **kwargs):
 
-    kwargs["source"]=kwargs.get("source","ultralytics/assets/bus.jpg")
-    model = YOLO(model_weight)
+	kwargs["source"]=kwargs.get("source","ultralytics/assets/bus.jpg")
+	
+	if isinstance(model_weight, str) and model_weight.endswith('.pt'):
+		model = YOLO(model_weight)
+	elif isinstance(model_weight,YOLOE) or isinstance(model_weight,YOLO):
+		model=model_weight
+	else:
+		raise ValueError("model_weight should be either a path to a .pt file or an instance of YOLOE/YOLO model.")
 
-    kwargs["names"]= kwargs.get("names",["bus","man"])
-    model.set_classes(kwargs["names"], model.get_text_pe(kwargs["names"]))
-    del kwargs["names"]
+	kwargs["names"]= kwargs.get("names",["bus","man"])
+	model.set_classes(kwargs["names"], model.get_text_pe(kwargs["names"]))
+	del kwargs["names"]
 
-    print(f"kwargs: {kwargs}")
-    res=model.predict(**kwargs)[0]
+	print(f"kwargs: {kwargs}")
+	res=model.predict(**kwargs)[0]
 
-    save_path=kwargs.get("save_path",f"./runs/temp/tp_{model_weight.replace('.pt','')}_pred.png")
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
-    res.save(save_path)
+	save_path=kwargs.get("save_path",f"./runs/temp/tp_{model_weight.replace('.pt','')}_pred.png")
+	os.makedirs(os.path.dirname(save_path), exist_ok=True)
+	res.save(save_path)
 
-    return save_path
+	return save_path
 
 
 def predict_yoloe_vp(model_weight="yoloe-26l-seg.pt",**kwargs):
 
-    model = YOLO(model_weight)
+	if isinstance(model_weight, str) and model_weight.endswith('.pt'):
+		model = YOLO(model_weight)
+	elif isinstance(model_weight,YOLOE) or isinstance(model_weight,YOLO):
+		model=model_weight
+	else:
+		raise ValueError("model_weight should be either a path to a .pt file or an instance of YOLOE/YOLO model.")
 
-    from ultralytics.models.yolo.yoloe import YOLOEVPSegPredictor
+	from ultralytics.models.yolo.yoloe import YOLOEVPSegPredictor
 
-    kwargs["source"]=kwargs.get("source",TestSample.get_visual_prompt(0)["image"])
-    kwargs["visual_prompts"]=kwargs.get("visual_prompts",TestSample.get_visual_prompt(0)["prompts"])
+	kwargs["source"]=kwargs.get("source",TestSample.get_visual_prompt(0)["image"])
+	kwargs["visual_prompts"]=kwargs.get("visual_prompts",TestSample.get_visual_prompt(0)["prompts"])
 
-    res = model.predict(
-        predictor=YOLOEVPSegPredictor,**kwargs)[0]
+	res = model.predict(
+		predictor=YOLOEVPSegPredictor,**kwargs)[0]
 
-    save_path=kwargs.get("save_path",f"./runs/temp/tp_{model_weight.replace('.pt','')}_pred.png")
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
-    res.save(save_path)
+	save_path=kwargs.get("save_path",f"./runs/temp/tp_{model_weight.replace('.pt','')}_pred.png")
+	os.makedirs(os.path.dirname(save_path), exist_ok=True)
+	res.save(save_path)
 
-    return save_path
+	return save_path
 
 import torch
 from ultralytics import YOLOE,YOLO
@@ -128,34 +141,34 @@ import os
 
 def print_model_head_cv3(model_weight):
 
-    model=YOLOE(model_weight)
-    model.args['clip_weight_name']="mobileclip2:b"
-    model_head=model.model.model[-1]
+	model=YOLOE(model_weight)
+	model.args['clip_weight_name']="mobileclip2:b"
+	model_head=model.model.model[-1]
 
-    for m in model_head.cv3:
-        print(m)
+	for m in model_head.cv3:
+		print(m)
 
 
 def print_model_head_cv4(model_weight):
-    print("*"*80)
-    print(f"Printing model head cv4 for {model_weight}")
-    model=YOLOE(model_weight)
-    model.args['clip_weight_name']="mobileclip2:b"
-    model_head=model.model.model[-1]
-    for m in model_head.cv4:
+	print("*"*80)
+	print(f"Printing model head cv4 for {model_weight}")
+	model=YOLOE(model_weight)
+	model.args['clip_weight_name']="mobileclip2:b"
+	model_head=model.model.model[-1]
+	for m in model_head.cv4:
 
-        #print(m.norm)# batch norm layer
-        print("BatchNorm running_mean:")
-        print(torch.max(m.norm.running_mean), torch.min(m.norm.running_mean), torch.mean(m.norm.running_mean))
-        print("BatchNorm running_var:")
-        print(torch.max(m.norm.running_var), torch.min(m.norm.running_var), torch.mean(m.norm.running_var))
-        print("BatchNorm weight:")
-        print(torch.max(m.norm.weight), torch.min(m.norm.weight), torch.mean(m.norm.weight))
-        print("BatchNorm bias:")
-        print(torch.max(m.norm.bias), torch.min(m.norm.bias), torch.mean(m.norm.bias))
+		#print(m.norm)# batch norm layer
+		print("BatchNorm running_mean:")
+		print(torch.max(m.norm.running_mean), torch.min(m.norm.running_mean), torch.mean(m.norm.running_mean))
+		print("BatchNorm running_var:")
+		print(torch.max(m.norm.running_var), torch.min(m.norm.running_var), torch.mean(m.norm.running_var))
+		print("BatchNorm weight:")
+		print(torch.max(m.norm.weight), torch.min(m.norm.weight), torch.mean(m.norm.weight))
+		print("BatchNorm bias:")
+		print(torch.max(m.norm.bias), torch.min(m.norm.bias), torch.mean(m.norm.bias))
 
 
-        print(f"logit_scale: {m.logit_scale.item()}")
-        print(f"bias: {m.bias}")
+		print(f"logit_scale: {m.logit_scale.item()}")
+		print(f"bias: {m.bias}")
 
 
